d6fbd24bc71746a636ffdd152d52e7412bcc8cc7
rezox <testikmoy@gmail.com>
2020-02-04 01:54:30 +0700

    Third test commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log("Hello world")
+process.env["NODE_CONFIG_DIR"] = '../config';
+const config = require('config');
+
+const app = require("express");
+
+const trello = require("trello");
+
+console.log(config.get("web.port"));
